4a5fb3d0cd0aca070f74f2b541a3d151fffe2e4c
kibervatnik <45829316+kibervatnik@users.noreply.github.com>
2019-02-27 02:11:18 +0300

    Update fib.php

--- fib.php
+++ fib.php
@@ -1,16 +1,15 @@
 <?php
 	$count=0;
-	$do=1;
-	$posle=1;
+	$first=1;
+	$second=1;
 	$now=0;
 	echo "1 1 ";
-	while ($count != 64)
+	while ($count != 62)
 	{
 		$count=$count+1;
-		$now=$do+$posle;
-		echo $now;
-		echo " ";
-		$do=$posle;
-		$posle=$now;
+		$now=$first+$second;
+		echo $now." ";
+		$first=$second;
+		$second=$now;
 	}
 ?>
